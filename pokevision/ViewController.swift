4c8ef747645877168bdf788e9c2dfa2badc5d299
zhhrozhh <zhangh40@msu.edu>
2016-07-23 23:36:05 -0400

    Initial Commit

---
+++ pokevision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokevision
+//
+//  Created by Hanghang Zhang on 7/23/16.
+//  Copyright © 2016 Hanghang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
